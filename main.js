bc03eca491d3515c19c8ebe4851c7741063fb511
Matteo Pompei <datxizzan@gmail.com>
2022-01-25 16:59:31 +0100

    Bonus fatto

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let app = new Vue({
   el: "#app",
   data: {
-    emailArr: [],
+    emailArr: []
   },
   methods: {
     request: function () {
@@ -9,16 +9,16 @@ let app = new Vue({
         .get("https://flynn.boolean.careers/exercises/api/random/mail")
         .then((response) => {
           console.log(response.data.response);
-          this.emailArr.push(response.data.response)
+          this.emailArr.push(response.data.response);
         })
         .catch((error) => {
           console.log(error);
         });
     },
   },
-  mounted: function() {
-    for (let i=0; i < 10; i++) {
-      this.request()
+  mounted: function () {
+    for (let i = 0; i < 10; i++) {
+      this.request();
     }
-  }
+  },
 });
